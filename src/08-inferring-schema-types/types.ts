b3ab31fc1d02d1fab72cec47b80f1b036f4ee788
Adrian Antanon <adrian.antanon@festo.com>
2022-12-28 14:21:59 +0100

    REFACTOR: use zod to create types

--- src/08-inferring-schema-types/types.ts
+++ src/08-inferring-schema-types/types.ts
@@ -1,16 +1,29 @@
-export type Pizza = {
-  name: string;
-  ingredients: string[];
-  price: number;
-  extras: string[];
-};
+import { z } from 'zod';
+import {
+  pizzaSchema,
+  extraIngredientSchema,
+  extraIngredientsSchema,
+} from './schemas';
 
-export type ExtraIngredients = Record<string, ExtraIngredient>;
+export type Pizza = z.infer<typeof pizzaSchema>;
 
-export type ExtraIngredient = {
-  name: string;
-  price: number;
-};
+// export type Pizza = {
+//   name: string;
+//   ingredients: string[];
+//   price: number;
+//   extras: string[];
+// };
+
+// export type ExtraIngredients = Record<string, ExtraIngredient>;
+
+// export type ExtraIngredient = {
+//   name: string;
+//   price: number;
+// };
+
+export type ExtraIngredients = z.infer<typeof extraIngredientsSchema>;
+
+export type ExtraIngredient = z.infer<typeof extraIngredientSchema>;
 
 export type PizzaOnOrder = {
   name: string;
